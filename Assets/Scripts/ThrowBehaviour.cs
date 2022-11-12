72da842d8caef825702886a00740df508d36c182
Mateo <mt399613@dal.ca>
2022-11-12 15:32:02 -0400

    Limited number of throwables the player has

--- Assets/Scripts/ThrowBehaviour.cs
+++ Assets/Scripts/ThrowBehaviour.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class ThrowBehaviour : MonoBehaviour {
 
     public GameObject throwableModel;
+    public int maxThrowables = 3; // the player can carry
     public float throwForce = 5f;
+
+    private int numberOfThrowables; // the player has
+
+    void Start() {
+        numberOfThrowables = maxThrowables;
+    }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetButtonDown("Fire3")) {
+        if (Input.GetButtonDown("Fire3") && numberOfThrowables > 0) {
             GameObject throwable = Instantiate(throwableModel, transform.position, transform.rotation);
             throwable.GetComponent<Rigidbody>().AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+            numberOfThrowables--;
         }
     }
 }
